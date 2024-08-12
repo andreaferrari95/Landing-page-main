fd1afe93f63010955436137e242d246f7c48f410
Andrea Ferrari | Phonetastik <131166715+andreaferrari95@users.noreply.github.com>
2024-08-12 12:06:34 +0200

    asd

--- alkaya_landing_page/src/components/Product Category/ProductsCategory.jsx
+++ alkaya_landing_page/src/components/Product Category/ProductsCategory.jsx
@@ -3,7 +3,6 @@ import "./ProductsCategory.css";
 import catEenergy from "../../assets/img/energy.png";
 import catProtection from "../../assets/img/protection.png";
 import catAudio from "../../assets/img/audio.png";
-import catData from "../../assets/img/data.png";
 import catGadgets from "../../assets/img/gadgets.png";
 
 export const ProductsCategory = () => {
@@ -13,7 +12,6 @@ export const ProductsCategory = () => {
         <img src={catEenergy} alt="" />
         <img src={catProtection} alt="" />
         <img src={catAudio} alt="" />
-        <img src={catData} alt="" />
         <img src={catGadgets} alt="" />
       </div>
     </div>
