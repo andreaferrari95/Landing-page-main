c3d0d7a264b103b2a316124d498421492a7e92a1
Andrea Ferrari | Phonetastik <131166715+andreaferrari95@users.noreply.github.com>
2024-08-12 12:57:06 +0200

    asd

--- alkaya_landing_page/src/App.jsx
+++ alkaya_landing_page/src/App.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { Navbar } from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import ProductsCategory from "./components/Product Category/ProductsCategory";
+import Title from "./components/Title/Title";
 
 export const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Title />
+      </div>
       <ProductsCategory />
     </div>
   );
---
+++ alkaya_landing_page/src/components/Title/Title.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Title = () => {
+  return (
+    <div className="title">
+      <h2>Discover alkaya.</h2>
+      <p>Our products by category</p>
+    </div>
+  );
+};
+
+export default Title;
