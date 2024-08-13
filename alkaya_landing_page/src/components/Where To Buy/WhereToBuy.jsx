185db697cf275e74a3966d089a8388685294d2d5
Andrea Ferrari | Phonetastik <131166715+andreaferrari95@users.noreply.github.com>
2024-08-13 16:37:15 +0200

    asd

--- alkaya_landing_page/src/components/Where To Buy/WhereToBuy.jsx
+++ alkaya_landing_page/src/components/Where To Buy/WhereToBuy.jsx
@@ -1,29 +1,40 @@
 import React from "react";
 import "./WhereToBuy.css";
+import otto from "../../assets/img/Where to Buy/Otto.png";
+import ebay from "../../assets/img/Where to Buy/eBay.png.png";
+import amazon from "../../assets/img/Where to Buy/amazon.png";
+import kaufland from "../../assets/img/Where to Buy/Kaufland.png";
+import etzy from "../../assets/img/Where to Buy/etzy.png";
 
 export const WhereToBuy = () => {
   return (
-    <div className="product-category">
-      <div className="prod-cat">
-        <img src={catEenergy} alt="" />
+    <div className="wtb-category">
+      <div className="wtb-cat">
+        <img src={otto} alt="" />
         <div className="caption">
           <button className="btn">Discover more</button>
         </div>
       </div>
-      <div className="prod-cat">
-        <img src={catProtection} alt="" />
+      <div className="wtb-cat">
+        <img src={ebay} alt="" />
         <div className="caption">
           <button className="btn">Discover more</button>
         </div>
       </div>
-      <div className="prod-cat">
-        <img src={catAudio} alt="" />
+      <div className="wtb-cat">
+        <img src={amazon} alt="" />
         <div className="caption">
           <button className="btn">Discover more</button>
         </div>
       </div>
-      <div className="prod-cat">
-        <img src={catGadgets} alt="" />
+      <div className="wtb-cat">
+        <img src={kaufland} alt="" />
+        <div className="caption">
+          <button className="btn">Discover more</button>
+        </div>
+      </div>
+      <div className="wtb-cat">
+        <img src={etzy} alt="" />
         <div className="caption">
           <button className="btn">Discover more</button>
         </div>
