36310640c575cb2025b6299463f41718342df749
Andrea Ferrari | Phonetastik <131166715+andreaferrari95@users.noreply.github.com>
2024-08-14 14:39:00 +0200

    asd

--- alkaya_landing_page/src/main.jsx
+++ alkaya_landing_page/src/main.jsx
@@ -3,12 +3,18 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "../src/utils/i18n";
-//import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import OurProducts from "./routes/OurProducts.jsx";
+import B2B from "./routes/B2B.jsx";
 
-//const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/our-products", element: <OurProducts /> },
+  { path: "/b2b", element: <B2B /> },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
