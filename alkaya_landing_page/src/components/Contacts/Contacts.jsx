95093ba6c248c16b07e6436f5c21e279c27cd697
Andrea Ferrari | Phonetastik <131166715+andreaferrari95@users.noreply.github.com>
2024-08-15 10:35:58 +0200

    asd

--- alkaya_landing_page/src/components/Contacts/Contacts.jsx
+++ alkaya_landing_page/src/components/Contacts/Contacts.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Contacts.css";
 
 export const Contacts = () => {
-  return <div>Contacts</div>;
+  return <div className="contact"></div>;
 };
 
 export default Contacts;
