9309e14b95385840082d3f098f10bcad09985b0a
Andrea Ferrari | Phonetastik <131166715+andreaferrari95@users.noreply.github.com>
2024-08-20 13:12:06 +0200

    asd

--- alkaya_landing_page/src/components/Footer/Footer.jsx
+++ alkaya_landing_page/src/components/Footer/Footer.jsx
@@ -1,13 +1,20 @@
 import "./Footer.css";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
     <div className="footer">
+      <p>© 2024 alkaya-One. All rights reserved.</p>
       <ul>
-        <li>aaaa</li>
-        <li>bbbb</li>
-        <li>ccccc</li>
-        <li>ccccc</li>
+        <li>
+          <Link to="/impressum">Impressum</Link>
+        </li>
+        <li>
+          <Link to="/agb">AGB</Link>
+        </li>
+        <li>
+          <Link to="datenschutz">Datenschutz</Link>
+        </li>
       </ul>
     </div>
   );
