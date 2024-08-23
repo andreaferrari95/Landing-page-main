3a1b7921b639c025665d5b3edbb199ab87756d18
Andrea Ferrari | Phonetastik <131166715+andreaferrari95@users.noreply.github.com>
2024-08-23 12:39:41 +0200

    asd

--- alkaya_landing_page/src/components/Navbar-Products/NavbarProducts.jsx
+++ alkaya_landing_page/src/components/Navbar-Products/NavbarProducts.jsx
@@ -1,10 +1,11 @@
 import "./NavbarProducts.css";
-
+import menuIcon from "../../assets/img/menu.svg";
 import logoWhite from "../../assets/img/logo-white.png";
 import { useTranslation } from "react-i18next";
 import germanflg from "../../assets/img/germany.png";
 import englishflg from "../../assets/img/united-kingdom.png";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export const NavbarProducts = () => {
   const { t, i18n } = useTranslation();
@@ -12,11 +13,15 @@ export const NavbarProducts = () => {
     { code: "en", name: "English", img: englishflg },
     { code: "de", name: "Deutch", img: germanflg },
   ];
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  };
 
   return (
     <nav className="prod-nav container dark-nav">
       <img src={logoWhite} alt="Logo" className="logo" />
-      <ul>
+      <ul className={mobileMenu ? "" : "hide-mobile-menu"}>
         <li>
           <Link to="/">Home</Link>
         </li>
@@ -38,18 +43,20 @@ export const NavbarProducts = () => {
             <button className="btn">{t("contacts")}</button>
           </Link>
         </li>
+        <li>
+          {" "}
+          {languages.map((language) => (
+            <button
+              className="btn-lang-main"
+              onClick={() => i18n.changeLanguage(language.code)}
+              key={language.code}
+            >
+              <img src={language.img} alt="" className="flag" />
+            </button>
+          ))}
+        </li>
       </ul>
-      <div>
-        {languages.map((language) => (
-          <button
-            className="prod-btn-lang"
-            onClick={() => i18n.changeLanguage(language.code)}
-            key={language.code}
-          >
-            <img src={language.img} alt="" className="flag" />
-          </button>
-        ))}
-      </div>
+      <img src={menuIcon} alt="" className="menu-icon" onClick={toggleMenu} />
     </nav>
   );
 };
