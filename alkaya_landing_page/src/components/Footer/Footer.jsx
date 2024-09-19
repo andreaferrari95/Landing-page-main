bcf85a06ec337b207db3d5d97d2ed629c59d6a78
Andrea Ferrari | Phonetastik <131166715+andreaferrari95@users.noreply.github.com>
2024-09-19 11:36:29 +0200

    social

--- alkaya_landing_page/src/components/Footer/Footer.jsx
+++ alkaya_landing_page/src/components/Footer/Footer.jsx
@@ -1,4 +1,7 @@
 import "./Footer.css";
+import tiktok from "../../assets/img/social/tiktok.svg";
+import facebook from "../../assets/img/social/facebook.svg";
+import instagram from "../../assets/img/social/instagram.svg";
 import { Link } from "react-router-dom";
 
 export const Footer = () => {
@@ -6,6 +9,21 @@ export const Footer = () => {
     <div className="footer">
       <p>© 2024 alkaya-One. All rights reserved.</p>
       <ul>
+        <li>
+          <a href="https://www.instagram.com/alkayaone/" target="blank">
+            <img src={instagram} alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.facebook.com/alkayaone" target="blank">
+            <img src={facebook} alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.tiktok.com/@alkayaone" target="blank">
+            <img src={tiktok} alt="" />
+          </a>
+        </li>
         <li>
           <Link to="/impressum">Impressum</Link>
         </li>
